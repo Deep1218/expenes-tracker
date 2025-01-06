67364142173a22e5def9c723419fca940e71d27a
Deep1218 <deeppatel18dec@gmail.com>
2025-01-06 17:07:35 +0530

    [feature] navigation added

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatIconModule, MatListModule],
+  imports: [MatIconModule, MatListModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
